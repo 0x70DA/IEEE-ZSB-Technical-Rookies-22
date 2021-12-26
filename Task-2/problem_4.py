df3e8c50924b2e997309d2e3b745a423eb13e3d7
Mahmoud Hussein <mahmoudh3257@gmail.com>
2021-12-26 20:45:43 +0200

    Remove the negative sign from min_distance

--- Task-2/problem_4.py
+++ Task-2/problem_4.py
@@ -4,11 +4,12 @@ import math
 
 def main():
     arr = list(map(int, input().split()))
-    min_distance = -math.inf  # Initially set to a very large number.
+    min_distance = math.inf  # Initially set to a very large number.
     for i in range(len(arr) - 1):
         for j in range(i+1, len(arr)):
             if arr[i] == arr[j]:
                 min_distance = min(min_distance, j - i)
+                break
     print(min_distance)
 
 
