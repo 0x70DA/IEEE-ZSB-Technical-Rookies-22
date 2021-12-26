1aebaf8bc01720c809150d75ae41abf8be5f6531
Mahmoud Hussein <mahmoudh3257@gmail.com>
2021-12-26 19:59:28 +0200

    Modify code

--- Task-2/problem_2.py
+++ Task-2/problem_2.py
@@ -2,8 +2,16 @@
 
 
 def main():
-    arr = set(map(int, input().split(' ')))
-    print(*arr)
+    arr = list(map(int, input().split(' ')))
+    distinct = [arr[0]]
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[j] == distinct[i]:
+            continue
+        distinct.append(arr[j])
+        i += 1
+    print(*distinct)
+
 
 
 if __name__ == '__main__':
